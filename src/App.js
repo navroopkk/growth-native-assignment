a85a8c8692d7891401e8f47db76812de62f0bd79
navroopkk <navroopkaran@gmail.com>
2022-12-21 22:09:15 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,32 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <table>
+        <tr>
+          <th>Roll no</th>
+          <th>Image</th>
+          <th>Total words</th>
+          <th>Total words</th>
+          <th>Total characters</th>
+          <th>words/minutes</th>
+          <th>characters</th>
+        </tr>
+        <tr>
+           <td>navroop</td>
+        </tr>
+        <tr>
+          
+        </tr>
+        <tr>
+          
+        </tr>
+      </table>
+    </div>
+   
+  );
+}
+
+export default App;
